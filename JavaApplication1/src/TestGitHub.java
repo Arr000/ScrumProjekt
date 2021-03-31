70fd8d9e9b07d87692ef694b0b9fabf0ecfc953f
Arr000 <arash_shahinzadeh@hotmail.com>
2021-03-31 14:50:11 +0200

    Öppnade en testklass med en konstruktor

---
+++ JavaApplication1/src/TestGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nene5
+ */
+public class TestGitHub {
+    
+    public TestGitHub()
+    {}
+    
+}
